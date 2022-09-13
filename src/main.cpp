846dc075628e59c24b969cf72ba0850282987e2a
bonkabel <abel.amy0@gmail.com>
2022-09-12 20:39:10 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+unsigned long myTime;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // start the USB serial monitor port
+  Serial.begin(115200);
+
+  // print some text to the USB port
+  Serial.println("\n\nHello!");  
+
+  Serial.println("\nName: Amy Abel");
+  Serial.println("Student ID: 000749165");
+
+  Serial.println("\nChip ID: " + String(ESP.getChipId()));
+  Serial.println("Flash Chip ID: " + String(ESP.getFlashChipId()));
+
+}
+
+void loop() {
+  myTime = millis();
+
+  Serial.println("\nMilliseconds since the D1 Mini was started:");
+
+  Serial.println(myTime);
+  delay(2000);
+}
